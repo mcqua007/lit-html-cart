f8dbb02dba8f4579bf9a865e6ac3b4a7347c5e4c
Sean McQuaid <sean@decrypteddesign.com>
2022-02-09 16:26:03 -0800

    feat | add styles and open cart function on button

--- components/cart-icon.js
+++ components/cart-icon.js
@@ -4,7 +4,20 @@ export class CartIcon extends LitElement {
   static styles = [
     css`
       :host {
-        display: block;
+      }
+      button {
+        border: none;
+        line-height: 1;
+        cursor: pointer;
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        font-weight: 600;
+        color: #2a2b2c;
       }
     `,
   ];
@@ -28,7 +41,12 @@ export class CartIcon extends LitElement {
   }
 
   render() {
-    return html` <span>Count: ${this.count}</span> `;
+    return html`<button @click=${this._onClick}>${this.count}</button>`;
+  }
+
+  _onClick(e) {
+    console.log('Click: ', e);
+    window.MicroBus.emit('cart-sidebar-toggle', {});
   }
 }
 
