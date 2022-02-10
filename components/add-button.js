2827428aec9589142d8884b9c31752dbc8283444
Sean McQuaid <sean@decrypteddesign.com>
2022-02-09 16:25:37 -0800

    feat | add button styles

--- components/add-button.js
+++ components/add-button.js
@@ -1,6 +1,28 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 
 export class AddButton extends LitElement {
+  static styles = [
+    css`
+      button {
+        border: none;
+        border-radius: 4px;
+        background-color: #2a2b2c;
+        color: #fff;
+        padding: 10px;
+        cursor: pointer;
+        transition: opacity 0.3s ease-in-out;
+      }
+
+      button:hover {
+        opacity: 0.85;
+      }
+
+      button:active {
+        opacity: 1;
+      }
+    `,
+  ];
+
   static properties = {
     pid: {type: Number},
     count: {type: Number},
